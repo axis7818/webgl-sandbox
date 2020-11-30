3e48d788b0b0377d443b3b357168a67e78ce189e
Cameron Taylor <axis7818@gmail.com>
2020-11-29 21:48:36 -0800

    texture

--- webgl.js
+++ webgl.js
@@ -1,5 +1,6 @@
 
 async function main() {
+	console.log("creating webgl context");
 	const canvas = document.querySelector("#glCanvas");
 	const gl = canvas.getContext("webgl");
 	if (gl === null) {
@@ -7,25 +8,31 @@ async function main() {
 		return;
 	}
 
+	console.log("loading shaders");
 	const loadVsSource = fetch('./shaders/vs.vert').then(r => r.text());
 	const loadFsSource = fetch('./shaders/fs.frag').then(r => r.text());
 	const [vsSource, fsSource] = await Promise.all([loadVsSource, loadFsSource]);
+
+	console.log("building shader program");
 	const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 	const programInfo = {
 		program: shaderProgram,
 		attribLocations: {
 			vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
-			vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
+			textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
 		},
 		uniformLocations: {
 			projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
 			modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+			uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
 		},
 	};
 
-	const buffers = initBuffers(gl);
+	console.log("initializing buffers");
+	const buffers = await initBuffers(gl);
 	let then = 0;
 	const render = (now) => {
+		// console.log("rendering");
 		now *= 0.001;
 		const deltaTime = now - then;
 		drawScene(gl, programInfo, buffers, deltaTime);
@@ -37,6 +44,7 @@ async function main() {
 /* Render the scene */
 
 function drawScene(gl, programInfo, buffers, deltaTime) {
+	// console.log("--- drawing scene");
 	gl.clearColor(0, 0, 0, 1);
 	gl.clearDepth(1);
 	gl.enable(gl.DEPTH_TEST);
@@ -44,6 +52,7 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+	// console.log("building perspecitve matrix");
 	const fieldOfView = 45 * Math.PI / 180;
 	const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
 	const zNear = 0.1;
@@ -51,12 +60,14 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 	const projectionMatrix = mat4.create();
 	mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
 
+	// console.log("building model view matrix");
 	const modelViewMatrix = mat4.create();
 	mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
 	const cubeRotation = deltaTime;
 	mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation, [0, 0, 1]);
 	mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * .7, [0, 1, 0]);
 
+	// console.log("binding position buffer");
 	{
 		const numComponents = 3;
 		const type = gl.FLOAT;
@@ -69,42 +80,86 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 		gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
 	}
 
+	// console.log("binding texture coordinate buffer");
 	{
-		const numComponents = 4;
+		const numComponents = 2;
 		const type = gl.FLOAT;
 		const normalize = false;
 		const stride = 0;
 		const offset = 0;
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-		gl.vertexAttribPointer(programInfo.attribLocations.vertexColor, numComponents, type,
+		gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+		gl.vertexAttribPointer(programInfo.attribLocations.textureCoord, numComponents, type,
 			normalize, stride, offset);
-		gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+		gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
 	}
 
+	// console.log("binding indices buffer");
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
 
+	// console.log("setting uniforms");
 	gl.useProgram(programInfo.program);
 	gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
 	gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
 
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, buffers.texture);
+	gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
+
+	// console.log("drawing elements");
 	{
 		const vertexCount = 36;
 		const type = gl.UNSIGNED_SHORT;
 		const offset = 0;
 		gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
 	}
+}
 
-	// {
-	// 	const offset = 0;
-	// 	const vertexCount = 4;
-	// 	gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-	// }
+/* Load Texture */
 
+function loadTexture(gl, url) {
+	const level = 0;
+	const internalFormat = gl.RGBA;
+	const srcFormat = gl.RGBA;
+	const srcType = gl.UNSIGNED_BYTE;
+
+	const image = new Image();
+	return new Promise((resolve, reject) => {
+		const timeout = setTimeout(() => reject(`Timed out loading the texture: ${url}`), 2000);
+
+		image.onload = () => {
+			const texture = gl.createTexture();
+			gl.bindTexture(gl.TEXTURE_2D, texture);
+			gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
+
+			if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+				gl.generateMipmap(gl.TEXTURE_2D);
+			} else {
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+			}
+
+			clearTimeout(timeout);
+			resolve(texture);
+		};
+
+		image.onerror = (err) => reject(err);
+
+		image.src = url;
+	});
+}
+
+function isPowerOf2(value) {
+	return (value & (value - 1)) == 0;
 }
 
 /* Create the initial Data */
 
-function initBuffers(gl) {
+async function initBuffers(gl) {
+	console.log("loading texture");
+	const texture = await loadTexture(gl, 'textures/cubetexture.png');
+
+	console.log("creating positions");
 	const positions = new Float32Array([
 		// Front face
 		-1.0, -1.0,  1.0,
@@ -146,23 +201,7 @@ function initBuffers(gl) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 
-	const faceColors = [
-		[1.0,  1.0,  1.0,  1.0], // Front face: white
-		[1.0,  0.0,  0.0,  1.0], // Back face: red
-		[0.0,  1.0,  0.0,  1.0], // Top face: green
-		[0.0,  0.0,  1.0,  1.0], // Bottom face: blue
-		[1.0,  1.0,  0.0,  1.0], // Right face: yellow
-		[1.0,  0.0,  1.0,  1.0], // Left face: purple
-	];
-	let colors = [];
-	for (let j = 0; j < faceColors.length; j += 1) {
-		const c = faceColors[j];
-		colors = colors.concat(c, c, c, c);
-	}
-	const colorBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
+	console.log("creating indices");
 	const indexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 	const indices = [
@@ -175,11 +214,48 @@ function initBuffers(gl) {
 	];
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
+	console.log("creating texture coordinates");
+	const textureCoordBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+	const textureCoordinates = [
+		// Front
+		0.0,  0.0,
+		1.0,  0.0,
+		1.0,  1.0,
+		0.0,  1.0,
+		// Back
+		0.0,  0.0,
+		1.0,  0.0,
+		1.0,  1.0,
+		0.0,  1.0,
+		// Top
+		0.0,  0.0,
+		1.0,  0.0,
+		1.0,  1.0,
+		0.0,  1.0,
+		// Bottom
+		0.0,  0.0,
+		1.0,  0.0,
+		1.0,  1.0,
+		0.0,  1.0,
+		// Right
+		0.0,  0.0,
+		1.0,  0.0,
+		1.0,  1.0,
+		0.0,  1.0,
+		// Left
+		0.0,  0.0,
+		1.0,  0.0,
+		1.0,  1.0,
+		0.0,  1.0,
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
 
 	return {
+		texture: texture,
 		position: positionBuffer,
-		color: colorBuffer,
 		indices: indexBuffer,
+		textureCoord: textureCoordBuffer,
 	};
 }
 
